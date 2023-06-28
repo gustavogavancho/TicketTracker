4a42a3271e71369613f536244c8770ddf2a470b4
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-06-27 22:17:45 -0500

    Improve paging algorithm

--- TicketTracker/Shared/Pagination/PagedList.cs
+++ TicketTracker/Shared/Pagination/PagedList.cs
@@ -17,7 +17,7 @@ public class PagedList<T> : List<T>
         AddRange(items);
     }
 
-    public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+    public static PagedList<T> ToPagedList(int totalItem, IEnumerable<T> source, int pageNumber, int pageSize)
     {
         var count = source.Count();
         var items = source
