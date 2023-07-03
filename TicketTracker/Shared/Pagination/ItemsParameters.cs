6b3f9f3beae65a695c1958ff655792e7b5587bd5
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-07-03 15:26:15 -0500

    Update data

--- TicketTracker/Shared/Pagination/ItemsParameters.cs
+++ TicketTracker/Shared/Pagination/ItemsParameters.cs
@@ -4,7 +4,7 @@ public class ItemsParameters
 {
     const int maxPageSize = 50;
     public int PageNumber { get; set; } = 1;
-    private int _pageSize = 30;
+    private int _pageSize = 20;
     public int PageSize
     {
         get
